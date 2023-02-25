285a91d98ec9f45467d0c11bf2f62b93def5f028
JaiNarain <jnarain2001@gmail.com>
2023-02-25 17:11:33 +0530

    navbar-name

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -64,7 +64,7 @@ function DrawerAppBar(props) {
             component="div"
             sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' } }}
           >
-            MUI
+            Conferencify
           </Typography>
           <Box sx={{ display: { xs: 'none', sm: 'block' } }}>
             {navItems.map((item) => (
